789df39038ea863876520c31c4809c23b2f863ee
nikita <nicita.zinkevitch@yandex.ru>
2024-01-30 14:07:59 +0300

    Add implementations for wallet to service and repo layers

--- internal/repository/wallet_postgres.go
+++ internal/repository/wallet_postgres.go
@@ -1,9 +1,33 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/Warh40k/infotecs_task/internal/domain"
+	"github.com/jmoiron/sqlx"
+	"github.com/shopspring/decimal"
+)
 
 type WalletPostgres struct {
 	db *sqlx.DB
+}
+
+func (w WalletPostgres) CreateWallet() (domain.Wallet, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (w WalletPostgres) GetWallet(id string) (domain.Wallet, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (w WalletPostgres) ShowHistory(id string) ([]domain.Transaction, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (w WalletPostgres) SendMoney(from, to string, amount decimal.Decimal) error {
+	//TODO implement me
+	panic("implement me")
 }
 
 func NewWalletPostgres(db *sqlx.DB) *WalletPostgres {
--- internal/service/wallet.go
+++ internal/service/wallet.go
@@ -1,1 +1,35 @@
 package service
+
+import (
+	"github.com/Warh40k/infotecs_task/internal/domain"
+	"github.com/Warh40k/infotecs_task/internal/repository"
+	"github.com/shopspring/decimal"
+)
+
+type WalletService struct {
+	repo repository.Wallet
+}
+
+func (w WalletService) CreateWallet() (domain.Wallet, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (w WalletService) GetWallet(id string) (domain.Wallet, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (w WalletService) ShowHistory(id string) ([]domain.Transaction, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (w WalletService) SendMoney(from, to string, amount decimal.Decimal) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewWalletService(repo repository.Wallet) *WalletService {
+	return &WalletService{repo: repo}
+}
