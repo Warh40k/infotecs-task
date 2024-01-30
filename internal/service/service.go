57d9af31317c53d21b4616e291d09e6b8641c961
nikita <nicita.zinkevitch@yandex.ru>
2024-01-30 14:07:35 +0300

    Add Wallet interface to service and repo layer

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,10 +1,16 @@
 package service
 
 import (
+	"github.com/Warh40k/infotecs_task/internal/domain"
 	"github.com/Warh40k/infotecs_task/internal/repository"
+	"github.com/shopspring/decimal"
 )
 
 type Wallet interface {
+	CreateWallet() (domain.Wallet, error)
+	GetWallet(id string) (domain.Wallet, error)
+	ShowHistory(id string) ([]domain.Transaction, error)
+	SendMoney(from, to string, amount decimal.Decimal) error
 }
 
 type Service struct {
@@ -13,6 +19,6 @@ type Service struct {
 
 func NewService(repos *repository.Repository) *Service {
 	return &Service{
-		Wallet: nil,
+		Wallet: NewWalletService(repos.Wallet),
 	}
 }
