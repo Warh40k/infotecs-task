6331761fe7028ef987ac595d0c1059cefc828ade
nikita <nicita.zinkevitch@yandex.ru>
2024-01-30 12:32:15 +0300

    Add main.go file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,50 @@
-package app
+package main
+
+import (
+	"github.com/Warh40k/infotecs_task/internal/app"
+	"github.com/Warh40k/infotecs_task/internal/handler"
+	"github.com/Warh40k/infotecs_task/internal/repository"
+	"github.com/Warh40k/infotecs_task/internal/service"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
+)
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("Ошибка чтения конфигурации: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("Ошибка чтения переменных окружения: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		logrus.Fatalf("Ошибка подключения к базе данных: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	serv := new(app.Server)
+	if err = serv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		logrus.Fatalf("Ошибка запуска http сервера: %s", err.Error())
+	}
+}
