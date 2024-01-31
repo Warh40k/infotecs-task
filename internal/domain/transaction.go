1545a986096197c964763c17a387b3f0726346c4
nikita <nicita.zinkevitch@yandex.ru>
2024-01-31 16:15:26 +0300

    Add transaction domain

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -1,10 +1,14 @@
 package domain
 
-import "github.com/shopspring/decimal"
+import (
+	"github.com/shopspring/decimal"
+	"time"
+)
 
 type Transaction struct {
-	Id     string
-	From   Wallet
-	To     Wallet
-	Amount decimal.Decimal
+	Id     string          `json:"id" db:"id"`
+	From   string          `json:"from" db:"from"`
+	To     string          `json:"to" db:"to"`
+	Amount decimal.Decimal `json:"amount" db:"amount"`
+	Time   time.Time       `json:"time" db:"time"`
 }
