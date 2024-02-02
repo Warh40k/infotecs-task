62cb19f2e589618cd3e63d3e0f12a14a2a8228de
nikita <nicita.zinkevitch@yandex.ru>
2024-02-02 12:09:49 +0300

    Implemented MarshalJson for Transaction

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -1,13 +1,29 @@
 package domain
 
 import (
+	"encoding/json"
 	"time"
 )
 
 type Transaction struct {
-	Time   time.Time `json:"time" db:"time"`
-	Id     string    `json:"-" db:"id"`
-	From   string    `json:"from" db:"from"`
-	To     string    `json:"to" db:"to"`
+	Time   time.Time `json:"time,string" db:"time"`
+	Id     string    `json:"-,string" db:"id"`
+	From   string    `json:"from,string" db:"from"`
+	To     string    `json:"to,string" db:"to"`
 	Amount float32   `json:"amount" db:"amount" binding:"gte=0"`
+}
+
+func (t Transaction) MarshalJSON() ([]byte, error) {
+	formattedTime := t.Time.Format(time.RFC3339)
+	return json.Marshal(struct {
+		Time   string  `json:"time"`
+		From   string  `json:"from"`
+		To     string  `json:"to"`
+		Amount float32 `json:"amount"`
+	}{
+		Time:   formattedTime,
+		From:   t.From,
+		To:     t.To,
+		Amount: t.Amount,
+	})
 }
