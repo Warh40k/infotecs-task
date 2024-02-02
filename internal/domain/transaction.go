11e69816c537242cef4f97fef14138760fab41a6
nikita <nicita.zinkevitch@yandex.ru>
2024-02-02 10:50:11 +0300

    Updated domains

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -1,14 +1,13 @@
 package domain
 
 import (
-	"github.com/shopspring/decimal"
 	"time"
 )
 
 type Transaction struct {
-	Id     string          `json:"id" db:"id"`
-	From   string          `json:"from" db:"from"`
-	To     string          `json:"to" db:"to"`
-	Amount decimal.Decimal `json:"amount" db:"amount"`
-	Time   time.Time       `json:"time" db:"time"`
+	Time   time.Time `json:"time" db:"time"`
+	Id     string    `json:"-" db:"id"`
+	From   string    `json:"from" db:"from"`
+	To     string    `json:"to" db:"to"`
+	Amount float32   `json:"amount" db:"amount"`
 }
--- internal/domain/wallet.go
+++ internal/domain/wallet.go
@@ -1,8 +1,6 @@
 package domain
 
-import "github.com/shopspring/decimal"
-
 type Wallet struct {
-	Id      string          `json:"id" db:"id"`
-	Balance decimal.Decimal `json:"balance" db:"balance"`
+	Id      string  `json:"id" db:"id"`
+	Balance float32 `json:"balance" db:"balance"`
 }
